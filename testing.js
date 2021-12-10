b7a14c6a727ef934008dbc930e4fbbf3f55a21c8
Caleb-MUGISHA <c.mugisha@alustudent>
2021-12-10 22:42:58 +0200

    kddkd

--- testing.js
+++ testing.js
@@ -3,4 +3,4 @@ var login = document.getElementById("input-submit");
 var gather_email1 = document.getElementById("gather_email1")
 var gather_password1 = document.getElementById("gather_password1")
 
-login.addEventListener("click", (e) => { console.log(gather_email1) })
+login.addEventListener("click", (e) => { console.log(gather_email1.value) })
