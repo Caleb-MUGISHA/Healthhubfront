d2a17ea34eefe0abaf783e8de4762be89eef089e
Caleb-MUGISHA <c.mugisha@alustudent>
2021-12-10 23:26:20 +0200

    kddkd

--- testing.js
+++ testing.js
@@ -1,5 +1,6 @@
-var login = document.getElementById("input-submit");
+require('axios');
 
+var login = document.getElementById("input-submit");
 var gather_email1 = document.getElementById("gather_email1")
 var gather_password1 = document.getElementById("gather_password1")
 
