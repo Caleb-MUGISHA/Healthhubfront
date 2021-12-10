d3ace96aa0f99f422fd1ce3c9376fa24a351a97f
Caleb-MUGISHA <c.mugisha@alustudent>
2021-12-10 23:16:11 +0200

    kddkd

--- testing.js
+++ testing.js
@@ -4,3 +4,16 @@ var gather_email1 = document.getElementById("gather_email1")
 var gather_password1 = document.getElementById("gather_password1")
 
 login.addEventListener("click", (e) => { console.log(gather_email1.value) })
+
+
+axios.post('/https://healthhubb.herokuapp.com/users/signin', {
+
+        emailaddress: gather_email1.value,
+        password: gather_password1.value
+    })
+    .then(function(response) {
+        console.log(response);
+    })
+    .catch(function(error) {
+        console.log(error);
+    });
