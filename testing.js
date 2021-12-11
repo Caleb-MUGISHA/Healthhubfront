ebd060cf6546e8d066e79e680135f6365f14cddf
Caleb-MUGISHA <c.mugisha@alustudent>
2021-12-11 14:35:48 +0200

    kdl

--- testing.js
+++ testing.js
@@ -5,7 +5,7 @@ var gather_email1 = document.getElementById("gather_email1")
 var gather_password1 = document.getElementById("gather_password1")
 
 login.addEventListener("submit", (e) => {
-    axios.post('/https://healthhubb.herokuapp.com/users/signin', {
+    axios.post('https://healthhubb.herokuapp.com/users/signin', {
 
         emailaddress: gather_email1.value,
         password: gather_password1.value
