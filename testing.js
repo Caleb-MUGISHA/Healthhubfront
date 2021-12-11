8bbdb84a40e635722adec1f7d33dbffa62efcb85
Caleb-MUGISHA <c.mugisha@alustudent>
2021-12-11 14:08:50 +0200

    rednder

--- testing.js
+++ testing.js
@@ -4,16 +4,20 @@ var login = document.getElementById("input-submit");
 var gather_email1 = document.getElementById("gather_email1")
 var gather_password1 = document.getElementById("gather_password1")
 
-login.addEventListener("click", (e) => {
+login.addEventListener("submit", (e) => {
     axios.post('/https://healthhubb.herokuapp.com/users/signin', {
 
-            emailaddress: gather_email1.value,
-            password: gather_password1.value
-        })
-        .then(function(response) {
-            console.log(response);
-        })
-        .catch(function(error) {
-            console.log(error);
-        });
+        emailaddress: gather_email1.value,
+        password: gather_password1.value
+    })
+
+    .then(function(response) {
+        console.log(response);
+    })
+
+    .catch(function(error) {
+        console.log(error);
+    });
+    render(home.php);
+
 })
