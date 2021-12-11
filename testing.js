aa59000539ed2c1ca59c65295c88f044e9eb90e8
Caleb-MUGISHA <c.mugisha@alustudent>
2021-12-11 18:35:54 +0200

    DOM1

--- testing.js
+++ testing.js
@@ -1,44 +1,8 @@
-require('axios');
+var login = document.getElementById("input-submit");
+// login.addEventListener("click", () => { console.log('wofofofoofo') }) 
+// var login = document.getElementById("input-submit");
+// login.addEventListener("click", () => { console.log('wofofofoofo') })
+var gather_email1 = document.getElementById("gather_email1")
+var gather_password1 = document.getElementById("gather_password1")
 
-// var login = document.querySelector("input-submit");
-// var gather_email1 = document.querySelector("gather_email1")
-// var gather_password1 = document.querySelector("gather_password1")
-
-// login.addEventListener("click", (e) => {
-
-
-//     console.log(gather_email1.value)
-console.log(gather_password1.value)
-    // axios.post('https://healthhubb.herokuapp.com/users/signin', {
-
-//     emailaddress: gather_email1.value,
-//     password: gather_password1.value
-// })
-
-// .then(function(response) {
-//     console.log(response);
-//     render(home.php);
-// })
-
-// .catch(function(error) {
-//     console.log(error);
-// });
-const form = document.querySelector('input-submit');
-
-const formEvent = form.addEventListener('submit', event => {
-    event.preventDefault();
-
-    const email = document.querySelector('#gather_email1').value;
-    const password = document.querySelector('#gather_password1').value;
-
-    // axios.post('https://healthhubb.herokuapp.com/users/signin', {
-
-    //     emailaddress: email,
-    //     password: password
-    // })
-    console.log(email);
-    console.log(password)
-
-});
-
-// })
+login.addEventListener("click", () => { console.log(gather_email1) })
